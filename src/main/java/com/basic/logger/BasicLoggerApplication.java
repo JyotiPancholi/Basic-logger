39475c18896061f0b422bab08263f90da8b00585
JyotiPancholi <jyotipancholi.adt@gmail.com>
2023-05-30 19:56:58 +0530

    basic logger code

---
+++ src/main/java/com/basic/logger/BasicLoggerApplication.java
@@ -0,0 +1,13 @@
+package com.basic.logger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicLoggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicLoggerApplication.class, args);
+	}
+
+}
